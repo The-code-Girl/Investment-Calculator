8ffe1099f2fd3317ceb7d51721396dfd086af698
Anjali <anjalijoshi710@gmail.com>
2023-08-23 00:00:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import Header from './Component/Header/Header';
+import ResultTable from './Component/ResultTable/ResultTable';
+import UserInput from './Component/UserInput/UserInput';
+
+function App() {
+  const [userInput, setUserInput] = useState(null);
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+  };
+
+  const yearlyData = [];
+  if (userInput) {
+    let currentSavings = +userInput['current-savings'];
+    const yearlyContribution = +userInput['yearly-contribution'];
+    const expectedReturn = +userInput['expected-return'] / 100;
+    const duration = +userInput['duration'];
+
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
+
+  return (
+    <div>
+      <Header />
+      <UserInput onCalculate={calculateHandler} />
+      {!userInput && <p style={{ textAlign: 'center' }}>No investment is calculated yet.</p>}
+      {userInput && <ResultTable input={yearlyData} initialInvestment={userInput['current-savings']} />}
+
+    </div>
+  );
+}
+
+export default App;
